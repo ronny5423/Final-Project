27200c44260c3f8fe7e9ec45159f748396a8456e
eaert <62422332+eaert@users.noreply.github.com>
2021-09-27 20:34:48 +0300

    Fixing Member component. Issue with params - Fixed.

--- src/components/pageComponents/About.js
+++ src/components/pageComponents/About.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import Member from '../sharedComponents/Member'
 
 export default function About() {
+    let img = "https://www.freeiconspng.com/uploads/person-icon-8.png";
+    let name = "Some One"
+    let job = "Doing Something"
+    let description = "At Somewhere"
     return (
         <div>
             <div>
-                <p>This is a Project UML, and writing more stuff so I could see the text box is working fine.</p>
+                <Card>
+                    <Card.Title>Project UML</Card.Title>
+                    <Card.Subtitle>Something Something</Card.Subtitle>
+                    <Card.Text>This is a Project UML, and writing more stuff so I could see the text box is working fine.</Card.Text>
+                </Card>
             </div>
             <div class="row">
-                <Member></Member>
-                <Member></Member>
+                <Member img={img} name={name} job={job} description={description}></Member>
             </div>
         </div>
     )
--- src/components/sharedComponents/Member.js
+++ src/components/sharedComponents/Member.js
@@ -5,14 +5,14 @@ import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import '../cssComponents/About.css'
 
-export default function Member() {
+export default function Member({img, name, job, description}) {
     return (
         <div class="col-lg-4">
             <div class="team-member">
-                <img class="mx-auto rounded-circle" src="" alt="" />
-                <h4>Larry Parker</h4>
-                <p class="text-muted">Lead Marketer</p>
-                <p>Working at Bla Bla at Bla Bla</p>
+                <img class="mx-auto rounded-circle" src={img} alt="" />
+                <h4>{name}</h4>
+                <p class="text-muted">{job}</p>
+                <p class="text-muted">{description}</p>
                 <a class="btn btn-dark btn-social mx-2" href="#!"><FontAwesomeIcon icon={faTwitter} /></a>
                 <a class="btn btn-dark btn-social mx-2" href="#!"><FontAwesomeIcon icon={faFacebookF} /></a>
                 <a class="btn btn-dark btn-social mx-2" href="#!"><FontAwesomeIcon icon={faLinkedinIn} /></a>
