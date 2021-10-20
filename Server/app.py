bc143dc538020cd16bf9d6a1dc0fc4cc6a5d1e3d
eaert <62422332+eaert@users.noreply.github.com>
2021-10-20 10:36:48 +0300

    Connecting Flask Server to React app.

--- Server/app.py
+++ Server/app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 import time
 
-app = Flask(__name__,
-            static_url_path='')
+app = Flask(__name__, static_url_path='')
 
 
 # app.config['SERVER_NAME'] = ''
 
 @app.route("/")
 def default():
-    return {'time:': time.time()}
+    return app.send_static_file('index.html')
 
 @app.route("/home")
 def home():
