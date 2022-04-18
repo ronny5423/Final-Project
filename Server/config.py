e99aeeb3864b76fd67efa83a1bf1afb7096a22a5
ilishmidt <73641268+ilishmidt@users.noreply.github.com>
2022-04-18 17:24:00 +0300

    Changing mongodb prod ports

--- Server/config.py
+++ Server/config.py
@@ -1,12 +1,12 @@
 class RunConfig:
     MONGO_HOST = 'localhost'
-    MONGO_PORT = 2717
+    MONGO_PORT = 27017
     MONGO_DBNAME = 'DBSelection'
     MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DBNAME}"
 
 
 class TestConfig:
     MONGO_HOST = 'localhost'
-    MONGO_PORT = 2718
+    MONGO_PORT = 27018
     MONGO_DBNAME = 'DBSelection'
     MONGO_URI = f"mongodb://{MONGO_HOST}:{MONGO_PORT}/{MONGO_DBNAME}"
